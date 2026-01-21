7d533acae8f4a1474a38c5b89436a39e611f02c8
William Chonzie <77587662+widycodes@users.noreply.github.com>
2026-01-21 12:52:51 +0200

    verision 1: without links

---
+++ assets/AboutView-OIlmUHqS.js
@@ -0,0 +1,1 @@
+import{_ as s,c as o,a as t,o as a}from"./index-Bwu3QJdO.js";const n={},c={class:"p-8 bg-blue-900"};function r(_,e){return a(),o("div",c,[...e[0]||(e[0]=[t("h1",null,"This is an about page",-1)])])}const l=s(n,[["render",r]]);export{l as default};
