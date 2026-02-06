2e18d71fea8855685ce1ce51022747f796390b9b
widycodes <chonziewilliam@gmail.com>
2026-02-06 14:25:52 +0000

    Update from https://github.com/widycodes/noble-tech-agriculture-frontend/commit/509e986632c9398e01c7d328f79b1c97b34ac3d5

--- assets/AboutView-DJsxCWa4.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as o,a as t,o as a}from"./index-r5BOG4Y6.js";const n={},c={class:"p-8 bg-blue-900"};function r(_,e){return a(),o("div",c,[...e[0]||(e[0]=[t("h1",null,"This is an about page",-1)])])}const l=s(n,[["render",r]]);export{l as default};
